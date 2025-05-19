ebb02248fa7a652599553c3e36b34808137529c3
TegistyH. <105536158+tigisthailay@users.noreply.github.com>
2025-05-19 20:31:36 +0300

    Total number of orders placed on public holiday of every month of previous year  query updated

--- postgres_data/Data2Bots/models/agg_public_holiday.sql
+++ postgres_data/Data2Bots/models/agg_public_holiday.sql
@@ -2,32 +2,27 @@
 Total number of orders placed on public holiday of every month of previous year
 */
 
-
-/* Declar variables*/
-DECLARE @previous_year int, @drived_dim_date DATE
-
-/* set values to the variables
-param: 
-- GETDATE(): to get current date of the database system.
-- DATEADD(): to get previous year.
-- YEAR(): to extract the Year part.
-*/
-
-SET @previous_year = YEAR(DATEADD(year, -1, GETDATE()))
-SET @drived_dim_date = (
-    SELECT CONCAT(year_num, '-', month_of_the_year_num, '-', day_of_the_month_num) FROM if_common.dim_dates
-    WHERE year_num = @previous_year AND day_of_the_week_num BETWEEN 1 AND 5 AND working_day = 'false'
-)
-
-with source as (
-    select *
-    from {{ref('feature')}}
+with public_holidays as (
+    select 
+        calender_dt,
+        month_of_the_year_num
+    from {{ ref('feature') }}
+    where 
+        year_num = YEAR(DATEADD(year, -1, GETDATE())) -- Previous year
+        and working_day = 'false' -- Non-working days (public holidays)
+        and day_of_the_week_num BETWEEN 1 AND 5
 ),
-destination as (
-    SELECT SUM(quantity) as tt_order_hol_month FROM tegidege9284_staging.orders, if_common.dim_dates
-    WHERE orders.order_date = @drived_dim_date
-    GROUP BY MONTH(orders.order_date)
-    ORDER BY MONTH(orders.order_date) ASC 
+orders_on_holidays as (
+    select 
+        d.month_of_the_year_num,
+        sum(f.quantity) as total_orders
+    from {{ ref('feature') }} f
+    inner join public_holidays d
+        on f.order_date = d.calender_dt
+    group by d.month_of_the_year_num
 )
-SELECT *
-FROM destination
+select 
+    month_of_the_year_num,
+    total_orders
+from orders_on_holidays
+order by month_of_the_year_num;
